61025903629afda3331adfd6ffe22717ac9989c2
Sumit.dev <sumitkharat.dev@gmail.com>
2024-04-14 10:58:36 +0530

    added find occurance of characters problem

---
+++ src/com/set1/OccOfCharacters.java
@@ -0,0 +1,29 @@
+package com.set1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class OccOfCharacters {
+
+	public static void main(String[] args) {
+		
+		HashMap<Character,Integer>map= new HashMap<>();
+		String str="Butter,Better";
+		
+		char strChar [] =str.toCharArray();
+		
+		for(char ch:strChar){
+			if(map.containsKey(ch)) {
+				map.put(ch, map.getOrDefault(ch, 0)+1);
+			}else {
+				map.put(ch, 1);
+			}
+		}
+		
+		//System.out.println(map);
+		
+		for(Map.Entry<Character, Integer> entry:map.entrySet()) {
+			System.out.println(entry.getKey()+" : "+entry.getValue());
+		}
+	}
+}
