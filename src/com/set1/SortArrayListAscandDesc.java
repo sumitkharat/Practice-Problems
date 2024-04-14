2bf60765828048e536c5fc4927546626bc03da83
Sumit.dev <sumitkharat.dev@gmail.com>
2024-04-14 10:02:09 +0530

    Added problem sort list by ascending and descending order

---
+++ src/com/set1/SortArrayListAscandDesc.java
@@ -0,0 +1,36 @@
+package com.set1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class SortArrayListAscandDesc {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> list= new ArrayList<>();
+		
+	list.add("Sumit");
+	list.add("Vilas");
+	list.add("Rani");
+	list.add("Karan");
+	list.add("Vandana");
+	list.add("Atharva");
+		
+	
+	System.out.println("Before Sorting");
+	
+	System.out.println(list);
+	System.out.println();
+	
+	System.out.println("After sorting ascending");
+	Collections.sort(list);
+	System.out.println(list);	
+	System.out.println();
+	
+	System.out.println("After sorting descending");
+	Collections.reverse(list);
+	System.out.println(list);
+	System.out.println();
+	
+	}
+}
